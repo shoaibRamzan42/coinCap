705f3b2a25428b0274ba10dc69e25c4656fb0043
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-03-10 19:39:54 +0500

    updated

--- src/Section3.jsx
+++ src/Section3.jsx
@@ -8,18 +8,18 @@ const Section3 = () => {
     <>
     <div className='py-5 border-b flex justify-between items-center font-custom '>
       <div className='flex gap-4 items-center'>
-        <button className='text-[#3861fb] font-bold  p-2 rounded-md  items-center bg-[#3861fb]/10 text-[12px] gap-1 flex'><img className='w-3 h-3' src="https://www.clipartmax.com/png/small/261-2613569_the-icon-is-a-square-composed-of-four-separate-sized-collage-icon.png" alt="pic" />
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'><img className='w-3 h-3' src="https://www.clipartmax.com/png/small/261-2613569_the-icon-is-a-square-composed-of-four-separate-sized-collage-icon.png" alt="pic" />
         Cryptocurrencies</button>
-        <button className='text-[#58667e] font-extrabold  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'><img className='w-3 h-3' src="https://www.pikpng.com/pngl/m/541-5410770_png-file-svg-categories-icon-png-clipart.png" alt="pic" />
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'><img className='w-3 h-3' src="https://www.pikpng.com/pngl/m/541-5410770_png-file-svg-categories-icon-png-clipart.png" alt="pic" />
         Categories</button>
         <h1>|</h1>
-        <button className='text-[#58667e] font-extrabold  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🏆
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🏆
         CMC Crypto Award 2024</button>
-        <button className='text-[#58667e] font-extrabold  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
         Solana Ecosystem</button>
-        <button className='text-[#58667e] font-extrabold  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
         Memes</button>
-        <button className='text-[#58667e] font-extrabold  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
+        <button className='text-[#58667e] font-extrabold focus:text-[#3861fb] duration-300 focus:bg-[#3861fb]/10  p-2 rounded-md  items-center hover:bg-gray-100 text-[12px] gap-1 flex'>🔥
         AI</button>
 
       
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PencilIcon } from "@heroicons/react/24/solid";
-
+import StarIcon from '@mui/icons-material/Star';
 import {
   ArrowDownTrayIcon,
   MagnifyingGlassIcon,
@@ -19,55 +19,77 @@ import {
   Input,
 } from "@material-tailwind/react";
  
-const TABLE_HEAD = ["Transaction", "Amount", "Date", "Status", "Account", ""];
+const TABLE_HEAD = ["","#","Name", "Price","1h%", "24h%", "7d%", "Market Cap",  "volume(24h)", "Last 7 days", ];
  
 const TABLE_ROWS = [
   {
-    img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
-    name: "Spotify",
-    amount: "$2,500",
-    date: "Wed 3:00pm",
-    status: "paid",
+    no:"1",
+    img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+    
+    logo:"BTC",
+    name: "Bitcoin",
+    
+    hr:"0.25%",
+    amount: "$68,767.01",
+    date: "$1,368,737,522,844",
+    thr: "1.70%",
+    days:"11.71%",
     account: "visa",
     accountNumber: "1234",
     expiry: "06/2026",
   },
   {
-    img: "https://docs.material-tailwind.com/img/logos/logo-amazon.svg",
-    name: "Amazon",
-    amount: "$5,000",
-    date: "Wed 1:00pm",
-    status: "paid",
+    no:"2",
+    hr:"0.25%",
+    img: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+    logo:"ETH",
+    name: "Ethereum",
+    amount: "$4,500",
+    date: "$471,754,636,228",
+    thr: "1.70%",
+    days:"11.71%",
     account: "master-card",
     accountNumber: "1234",
     expiry: "06/2026",
   },
   {
-    img: "https://docs.material-tailwind.com/img/logos/logo-pinterest.svg",
-    name: "Pinterest",
-    amount: "$3,400",
-    date: "Mon 7:40pm",
-    status: "pending",
+    no:"3",
+    hr:"0.25%",
+    img: "https://cryptologos.cc/logos/bnb-bnb-logo.png",
+    logo:"BNB",
+    name: "BNB",
+    amount: "$531.78",
+    date: "$79,353,878,387",
+    thr: "0.91%",
+    days:"11.71%",
     account: "master-card",
     accountNumber: "1234",
     expiry: "06/2026",
   },
   {
-    img: "https://docs.material-tailwind.com/img/logos/logo-google.svg",
-    name: "Google",
-    amount: "$1,000",
-    date: "Wed 5:00pm",
-    status: "paid",
+    no:"4",
+    hr:"0.25%",
+    img: "https://www.liblogo.com/img-logo/so2809sc45-solana-logo-solana-blockchain-platform-liblogo.png",
+    logo:"SOL",
+    name: "Solana",
+    amount: "$145.60",
+    date: "$64,201,600,845",
+    thr: "1.70%",
+    days:"11.71%",
     account: "visa",
     accountNumber: "1234",
     expiry: "06/2026",
   },
   {
-    img: "https://docs.material-tailwind.com/img/logos/logo-netflix.svg",
-    name: "netflix",
-    amount: "$14,000",
-    date: "Wed 3:30am",
-    status: "cancelled",
+    no:"5",
+    hr:"0.25%",
+    img: "https://altcoinsbox.com/wp-content/uploads/2023/01/xrp-logo.png",
+    logo:"XRP",
+    name: "XRP",
+    amount: "$0.6576",
+    date: "$33,769,301,085",
+    thr: "1.56%",
+    days:"11.71%",
     account: "visa",
     accountNumber: "1234",
     expiry: "06/2026",
@@ -76,11 +98,11 @@ const TABLE_ROWS = [
  
 export function TransactionsTable() {
   return (
-    <Card className="h-full no-scrollbar w-full">
+    <Card className="h-full font-custom no-scrollbar w-full">
       <CardHeader floated={false} shadow={false} className="rounded-none">
         <div className=" flex flex-col justify-between gap-8 md:flex-row  md:items-center ">
           
-          <div className="flex w-full shrink-0 gap-2 md:w-max">
+          <div className="flex w-full pt-1 shrink-0 gap-2 md:w-max">
             <div className="w-full md:w-72">
               <Input
                 label="Search"
@@ -102,8 +124,8 @@ export function TransactionsTable() {
                 >
                   <Typography
                     variant="small"
-                    color="blue-gray"
-                    className="font-normal leading-none opacity-70"
+                    color="black"
+                    className="font-bold leading-none opacity-70"
                   >
                     {head}
                   </Typography>
@@ -115,11 +137,15 @@ export function TransactionsTable() {
             {TABLE_ROWS.map(
               (
                 {
+                  no,
+
                   img,
+                  logo,
                   name,
                   amount,
-                  date,
-                  status,
+                  hr,
+                  thr,
+                  days,
                   account,
                   accountNumber,
                   expiry,
@@ -133,28 +159,54 @@ export function TransactionsTable() {
  
                 return (
                   <tr key={name}>
+                  <td className={classes}>
+                      
+                        
+                          <StarIcon className="h-4 w-4 hover:bg-gray-200 rounded-lg focus:text-orange-500 text-gray-400" />
+                        
+                      
+                    </td>
+                  <td className={classes}>
+                  
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-semibold"
+                      >
+                        {no}
+                      </Typography>
+                    </td>
                     <td className={classes}>
+                    
                       <div className="flex items-center gap-3">
+                      
                         <Avatar
                           src={img}
                           alt={name}
                           size="md"
-                          className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
+                          className="  object-contain p-1"
                         />
                         <Typography
                           variant="small"
-                          color="blue-gray"
+                          color="black"
                           className="font-bold"
                         >
                           {name}
+                        </Typography>
+                        <Typography
+                          variant="small"
+                          
+                          className="font-bold text-[#808a9d]"
+                        >
+                          {logo}
                         </Typography>
                       </div>
                     </td>
                     <td className={classes}>
                       <Typography
-                        variant="small"
+                        variant="h6"
                         color="blue-gray"
-                        className="font-normal"
+                        className="font-semibold"
                       >
                         {amount}
                       </Typography>
@@ -162,50 +214,55 @@ export function TransactionsTable() {
                     <td className={classes}>
                       <Typography
                         variant="small"
-                        color="blue-gray"
-                        className="font-normal"
+                        color="red"
+                        className="font-semibold"
                       >
-                        {date}
+                        {hr}
                       </Typography>
                     </td>
                     <td className={classes}>
                       <div className="w-max">
-                        <Chip
-                          size="sm"
-                          variant="ghost"
-                          value={status}
-                          color={
-                            status === "paid"
-                              ? "green"
-                              : status === "pending"
-                              ? "amber"
-                              : "red"
-                          }
-                        />
+                      <Typography
+                        variant="small"
+                        
+                        className="font-semibold text-[#16c784]"
+                      >
+                        {thr}
+                      </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="w-max">
+                      <Typography
+                        variant="small"
+                        
+                        className="font-semibold text-[#16c784]"
+                      >
+                        {days}
+                      </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="w-max">
+                      <Typography
+                        variant="small"
+                        color="black"
+                        className="font-semibold "
+                      >
+                        {account}
+                      </Typography>
                       </div>
                     </td>
                     <td className={classes}>
                       <div className="flex items-center gap-3">
-                        <div className="h-9 w-12 rounded-md border border-blue-gray-50 p-1">
-                          <Avatar
-                            src={
-                              account === "visa"
-                                ? "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/visa.png"
-                                : "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/mastercard.png"
-                            }
-                            size="sm"
-                            alt={account}
-                            variant="square"
-                            className="h-full w-full object-contain p-1"
-                          />
-                        </div>
+                        
                         <div className="flex flex-col">
                           <Typography
                             variant="small"
                             color="blue-gray"
                             className="font-normal capitalize"
                           >
-                            {account.split("-").join(" ")} {accountNumber}
+                             {accountNumber}
                           </Typography>
                           <Typography
                             variant="small"
